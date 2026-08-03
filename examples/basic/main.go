f42c4935c8a3aa7728f9b8e070c770794a61cbcc
Dively_HP <milwad.dev@gmail.com>
2026-08-03 17:52:35 +0330

    Create main.go

---
+++ examples/basic/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"log"
+
+	stub "github.com/binafy/go-stub"
+)
+
+//go:embed model.stub
+var stubs embed.FS
+
+func main() {
+	name := "User"
+
+	// Functional API, reading the stub from the embedded filesystem.
+	out, err := stub.RenderFS(stubs, "model.stub",
+		stub.WithReplaces(map[string]any{
+			"PACKAGE": stub.ToSnake(name) + "s", // "users"
+			"NAME":    stub.ToPascal(name),      // "User"
+		}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("--- functional (RenderFS) ---")
+	fmt.Println(out)
+
+	// Fluent API, same source and values.
+	out, err = stub.New().
+		FromFS(stubs, "model.stub").
+		Replace("PACKAGE", "users").
+		Replace("NAME", "User").
+		Render()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("--- fluent (New) ---")
+	fmt.Println(out)
+}
