ad98a22e6a573309a255894ffc4587353e7a983a
Dively_HP <milwad.dev@gmail.com>
2026-08-04 18:57:33 +0330

    Update example_test.go

--- example_test.go
+++ example_test.go
@@ -1,9 +1,11 @@
 package stub_test
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 
 	stub "github.com/binafy/go-stub"
 )
@@ -63,4 +65,82 @@ func ExampleToSnake() {
 	// Output:
 	// http_server
 	// UserName
+}
+
+func ExampleRenderFS() {
+	// embedded is an embed.FS declared in the test package.
+	out, err := stub.RenderFS(embedded, "testdata/model.stub",
+		stub.WithReplaces(map[string]any{"PACKAGE": "models", "NAME": "Account"}),
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out[:14])
+	// Output: package models
+}
+
+func ExampleGenerateDir() {
+	dstDir := filepath.Join(os.TempDir(), "go-stub-example-dir")
+	defer func() { _ = os.RemoveAll(dstDir) }()
+
+	err := stub.GenerateDir("testdata/scaffold", dstDir,
+		stub.WithReplaces(map[string]any{"PACKAGE": "user", "NAME": "User"}),
+		stub.WithTrimSuffix(".stub"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the generated tree, using forward slashes so the output is the
+	// same on every OS.
+	var files []string
+	_ = filepath.Walk(dstDir, func(path string, info os.FileInfo, err error) error {
+		if err == nil && !info.IsDir() {
+			rel, _ := filepath.Rel(dstDir, path)
+			files = append(files, filepath.ToSlash(rel))
+		}
+		return nil
+	})
+	sort.Strings(files)
+	for _, f := range files {
+		fmt.Println(f)
+	}
+	// Output:
+	// User.go
+	// inner/note.txt
+}
+
+func ExampleGenerateJobs() {
+	dir := filepath.Join(os.TempDir(), "go-stub-example-jobs")
+	defer func() { _ = os.RemoveAll(dir) }()
+
+	err := stub.GenerateJobs([]stub.Job{
+		{Src: "testdata/model.stub", Dst: filepath.Join(dir, "user.go"),
+			Opts: []stub.Option{stub.WithReplace("NAME", "User")}},
+		{Src: "testdata/model.stub", Dst: filepath.Join(dir, "account.go"),
+			Opts: []stub.Option{stub.WithReplace("NAME", "Account")}},
+	}, stub.WithReplace("PACKAGE", "models")) // shared by every job
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("generated 2 files")
+	// Output: generated 2 files
+}
+
+func ExampleWithStrict() {
+	// The stub needs NAME, but it is not provided.
+	_, err := stub.Render("testdata/model.stub",
+		stub.WithReplace("PACKAGE", "models"),
+		stub.WithStrict(),
+	)
+
+	var mk *stub.MissingKeysError
+	if errors.As(err, &mk) {
+		fmt.Println("missing:", mk.Keys)
+	}
+	fmt.Println("is ErrMissingKeys:", errors.Is(err, stub.ErrMissingKeys))
+	// Output:
+	// missing: [NAME]
+	// is ErrMissingKeys: true
 }
